190045582bae0091589243dc1ef08f20300edd38
Alex Jones <alexsimonjones@gmail.com>
2023-02-24 21:23:07 +0000

    wip
    Signed-off-by: Alex Jones <alexsimonjones@gmail.com>

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,11 +1,30 @@
 use std::collections::HashMap;
 
+#[derive(Clone)]
 pub struct EvaluationContext {
     pub targetting_key: String,
     pub attributes: HashMap<String,String>,
 }
+
+type FlattenedContext = HashMap<String,String>;
+
+pub fn flatten_context(context: EvaluationContext) -> FlattenedContext {
+    let mut flattened_context = HashMap::new();
+    flattened_context.insert(context.targetting_key(), context.attribute(context.targetting_key()));
+    for (key, value) in context.attributes() {
+        flattened_context.insert(key, value);
+    }
+    flattened_context
+}
 // impl
 impl EvaluationContext {
+    pub fn new(targetting_key: String, 
+        attributes: HashMap<String,String>) -> Self {
+        Self {
+            targetting_key: targetting_key,
+            attributes: attributes
+        }
+    }
     fn attribute(&self, key: String) -> String {
         self.attributes.get(&key).unwrap().clone()
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,158 +1,212 @@
+use std::{fmt::Error, collections::HashMap};
+
 use anyhow::Result;
 
 pub mod components;
+
+enum Type {
+    Bool,
+    String,
+    Float,
+    Int,
+}
 trait ClientTraits {
-    fn meta_data() -> ClientMetaData;
-    fn set_evaluation_context(eval_ctx: components::EvaluationContext);
-    fn evaluation_context() -> components::EvaluationContext;
-    fn boolean_value(flag: String, default_value: bool, eval_ctx: components::EvaluationContext) -> Result<bool>;
-    fn string_value(flag: String, default_value: String, eval_ctx: components::EvaluationContext) -> Result<String>;
-    fn float_value(flag: String, default_value: f64, eval_ctx: components::EvaluationContext) -> Result<f64>;
-    fn int_value(flag: String, default_value: i64, eval_ctx: components::EvaluationContext) -> Result<i64>;
-    fn boolean_value_details(flag: String, default_value: String, eval_ctx: components::EvaluationContext) -> (bool_evaluation_details,Result<bool>);
-    fn string_value_details(flag: String, default_value: String, eval_ctx: components::EvaluationContext) -> (string_evaluation_details,Result<String>);
-    fn float_value_details(flag: String, default_value: f64, eval_ctx: components::EvaluationContext) -> (float_evaluation_details,Result<f64>);
-    fn int_value_details(flag: String, default_value: i64, eval_ctx: components::EvaluationContext) -> (int_evaluation_details,Result<i64>);
+
+    fn meta_data(&self) -> ClientMetaData;
+    fn set_evaluation_context(&mut self,eval_ctx: components::EvaluationContext);
+    fn evaluation_context(&self) -> components::EvaluationContext;
+    fn boolean_value(&self,flag: String, default_value: bool, eval_ctx: components::EvaluationContext) -> Result<bool>;
+    fn string_value(&self,flag: String, default_value: String, eval_ctx: components::EvaluationContext) -> Result<String>;
+    fn float_value(&self,flag: String, default_value: f64, eval_ctx: components::EvaluationContext) -> Result<f64>;
+    fn int_value(&self,flag: String, default_value: i64, eval_ctx: components::EvaluationContext) -> Result<i64>;
+    fn boolean_value_details(&self,flag: String, default_value: String, eval_ctx: components::EvaluationContext) -> (BoolEvaluationDetails,Result<bool>);
+    fn string_value_details(&self,flag: String, default_value: String, eval_ctx: components::EvaluationContext) -> (StringEvaluationDetails,Result<String>);
+    fn float_value_details(&self,flag: String, default_value: f64, eval_ctx: components::EvaluationContext) -> (FloatEvaluationDetails,Result<f64>);
+    fn int_value_details(&self,flag: String, default_value: i64, eval_ctx: components::EvaluationContext) -> (IntEvaluationDetails,Result<i64>);
 }
 struct Client {  
     meta_data: ClientMetaData,
-    evaluation_context: components::EvaluationContext
+    evaluation_context: components::EvaluationContext,
 }
+#[derive(Clone)]
 struct ClientMetaData {
     name: String
 }
-struct bool_evaluation_details {
+struct BoolEvaluationDetails {
     value: bool,
     flag_key: String,
-    flag_type: String,
+    flag_type: Type,
     variant: String,
     reason: String,
     error_code: String,
     error_message: String,
 }
-struct string_evaluation_details {
+struct StringEvaluationDetails {
     value: String,
     flag_key: String,
-    flag_type: String,
+    flag_type: Type,
     variant: String,
     reason: String,
     error_code: String,
     error_message: String,
 }
-struct float_evaluation_details {
+struct FloatEvaluationDetails {
     value: f64,
     flag_key: String,
-    flag_type: String,
+    flag_type: Type,
     variant: String,
     reason: String,
     error_code: String,
     error_message: String,
 }
-struct int_evaluation_details {
+struct IntEvaluationDetails {
     value: i64,
     flag_key: String,
-    flag_type: String,
+    flag_type: Type,
+    variant: String,
+    reason: String,
+    error_code: String,
+    error_message: String,
+}
+struct DynamicEvaluationDetails<T> {
+    value: T,
+    flag_key: String,
+    flag_type: Type,
     variant: String,
     reason: String,
     error_code: String,
     error_message: String,
 }
 // Client impl
+impl Client {
+    pub fn new(meta_data: ClientMetaData, 
+        evaluation_context: components::EvaluationContext) -> Self {
+        Self {
+            meta_data: meta_data,
+            evaluation_context: evaluation_context
+        }
+    }
+    pub fn evaluate<T>(flag: String, flagType: Type, defaultValue: T,
+         eval_ctx: components::EvaluationContext) -> Result<String,Error> {
+             
+            let eval_details = DynamicEvaluationDetails {
+                value: defaultValue,
+                flag_key: flag,
+                flag_type: flagType,
+                variant: todo!(),
+                reason: todo!(),
+                error_code: todo!(),
+                error_message: todo!(),
+            };
+
+            let flat_ctx = components::flatten_context(eval_ctx);
+
+        
+            // match flag type
+            match flagType {
+                Type::String => {
+    
+                },            
+                Type::Bool => {
+    
+                }, 
+                Type::Float => {
+    
+                },
+                Type::Int => {
+    
+                },               
+            }
+
+
+            // Rust has some really sucky parts to it, 
+            // I am not adding a trait to do this, here be dragons
+
+
+       
+            return Ok("".to_string());
+    }
+}
 impl ClientTraits for Client {
-    fn meta_data() -> ClientMetaData {
+    // Return metadata
+    fn meta_data(&self) -> ClientMetaData {
+        return self.meta_data.clone();
+    }
+
+    fn set_evaluation_context(&mut self, eval_ctx: components::EvaluationContext) {
+        self.evaluation_context = eval_ctx;
+    }
+
+    fn evaluation_context(&self) -> components::EvaluationContext {
+        return self.evaluation_context.clone();
+    }
+
+    fn boolean_value(&self,flag: String, default_value: bool, eval_ctx: components::EvaluationContext) -> Result<bool> {
         todo!()
     }
 
-    fn set_evaluation_context(eval_ctx: components::EvaluationContext) {
+    fn string_value(&self,flag: String, default_value: String, eval_ctx: components::EvaluationContext) -> Result<String> {
         todo!()
     }
 
-    fn evaluation_context() -> components::EvaluationContext {
+    fn float_value(&self,flag: String, default_value: f64, eval_ctx: components::EvaluationContext) -> Result<f64> {
         todo!()
     }
 
-    fn boolean_value(flag: String, default_value: bool, eval_ctx: components::EvaluationContext) -> Result<bool> {
+    fn int_value(&self,flag: String, default_value: i64, eval_ctx: components::EvaluationContext) -> Result<i64> {
         todo!()
     }
 
-    fn string_value(flag: String, default_value: String, eval_ctx: components::EvaluationContext) -> Result<String> {
+    fn boolean_value_details(&self,flag: String, default_value: String, eval_ctx: components::EvaluationContext) -> (BoolEvaluationDetails,Result<bool>) {
         todo!()
     }
 
-    fn float_value(flag: String, default_value: f64, eval_ctx: components::EvaluationContext) -> Result<f64> {
+    fn string_value_details(&self,flag: String, default_value: String, eval_ctx: components::EvaluationContext) -> (StringEvaluationDetails,Result<String>) {
         todo!()
     }
 
-    fn int_value(flag: String, default_value: i64, eval_ctx: components::EvaluationContext) -> Result<i64> {
+    fn float_value_details(&self,flag: String, default_value: f64, eval_ctx: components::EvaluationContext) -> (FloatEvaluationDetails,Result<f64>) {
         todo!()
     }
 
-    fn boolean_value_details(flag: String, default_value: String, eval_ctx: components::EvaluationContext) -> (bool_evaluation_details,Result<bool>) {
+    fn int_value_details(&self,flag: String, default_value: i64, eval_ctx: components::EvaluationContext) -> (IntEvaluationDetails,Result<i64>) {
         todo!()
     }
 
-    fn string_value_details(flag: String, default_value: String, eval_ctx: components::EvaluationContext) -> (string_evaluation_details,Result<String>) {
-        todo!()
-    }
-
-    fn float_value_details(flag: String, default_value: f64, eval_ctx: components::EvaluationContext) -> (float_evaluation_details,Result<f64>) {
-        todo!()
-    }
-
-    fn int_value_details(flag: String, default_value: i64, eval_ctx: components::EvaluationContext) -> (int_evaluation_details,Result<i64>) {
-        todo!()
-    }
 }
 
-fn new_client(meta_data: ClientMetaData, evaluation_context: components::EvaluationContext) -> Client {
-    Client {
-        meta_data: meta_data,
-        evaluation_context: evaluation_context
-    }
-}
 
 // ClientMetaData impl
 impl ClientMetaData {
+    pub fn new(name: String) -> Self {
+        Self {
+            name: name
+        }
+    }
     fn get_name(&self) -> String {
         self.name.clone()
     }
+ 
 }
-fn new_client_meta_data(name: String) -> ClientMetaData {
-    ClientMetaData {
-        name: name
-    }
-}
+
 
 #[cfg(test)]
 mod tests {
-    use crate::{new_client_meta_data, Client, components, new_client};
+    use crate::{Client, ClientMetaData, components};
 
     #[test]
     fn test_set_name() {
-        let client_meta_data = new_client_meta_data("test".to_string());
-        // assert set
+        let client_meta_data = ClientMetaData::new("test".to_string());
         assert_eq!(client_meta_data.get_name(), "test");
     }
     #[test]
     fn test_client_impl() {
         // assert client impl
-        let client = Client{
-            meta_data: new_client_meta_data("test".to_string()),
-            evaluation_context: components::EvaluationContext{
-                targetting_key: "test".to_string(),
-                attributes: std::collections::HashMap::new()
-            }
-        };
+        
+        let client = Client::new(ClientMetaData { name: ("test").to_string() }, 
+            components::EvaluationContext{ targetting_key: todo!(), attributes: todo!() });
         assert_eq!(client.meta_data.get_name(), "test");
     }
-    #[test]
-    fn test_client() {
-        // assert client
-        let client = new_client(new_client_meta_data("test".to_string()), components::EvaluationContext{
-            targetting_key: "test".to_string(),
-            attributes: std::collections::HashMap::new()
-        });
-        assert_eq!(client.meta_data.get_name(), "test");
-    }
+ 
 
 }
